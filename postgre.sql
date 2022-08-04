47a0471d19fa7e1dbcd3e29a904ed38b15fc9ab4
Ziqian Li <liziqian1992@gmail.com>
2022-08-03 19:46:03 -0700

    create postgresql schema

--- postgre.sql
+++ postgre.sql
@@ -4,3 +4,33 @@ CREATE DATABASE hr_sdc;
 DROP SCHEMA IF EXISTS reviews CASCADE;
 CREATE SCHEMA reviews;
 
+CREATE TABLE reviews.products (
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  slogan varchar(255),
+  description varchar(255),
+  category varchar(255),
+  default_price varchar(255)
+);
+
+CREATE TABLE reviews.reviews (
+  id SERIAL PRIMARY KEY,
+  name varchar(255),
+  email varchar(255),
+  rating integer,
+  summary varchar(255),
+  recommend boolean,
+  body varchar(255),
+  date varchar(255),
+  characteristics varchar(255),
+  response varchar(255) NULL,
+  helpfulness INT DEFAULT 0,
+  report BOOLEAN DEFAULT FALSE,
+  product_id integer references reviews.products
+);
+
+CREATE TABLE reviews.photos (
+  id SERIAL PRIMARY KEY,
+  url varchar(255),
+  reviews_id integer references reviews.reviews(id)
+);
