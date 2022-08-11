fddf5767b9b2b3f11d38a628a7f3d04a284af9bb
Ziqian Li <liziqian1992@gmail.com>
2022-08-11 15:13:13 -0700

    load characteristics table

--- etl_system/index.js
+++ etl_system/index.js
@@ -1,4 +1,5 @@
 const csv = require('csv');
+// const { Client } = require('pg');
 
 const pool = require('../primaryDB/index');
 const extract = require('./extract');
@@ -20,14 +21,48 @@ const options = {
     colNames: 'id,review_id,url',
     numOfCols: 3,
   },
+  characteristics: {
+    tableName: 'hr_sdc.characteristics',
+    colNames: 'id,product_id,name',
+    numOfCols: 3,
+  },
 };
 
-const basicETL = (fileName) => {
+// const client = new Client({
+//   user: 'ziqianli',
+//   host: 'localhost',
+//   database: 'ziqianli',
+//   port: 5432,
+// });
+// client.connect();
+
+// const basicETL = (fileName) => {
+//   extract.getInputFileStream(fileName)
+//     .pipe(csv.parse({ delimiter: ',', from_line: 2 }))
+//     .on('data', async (row) => {
+//       await load.insertOne(pool, row, options[fileName]);
+//     })
+//     .on('end', () => {
+//       console.log(`${fileName} reading finished!`);
+//     })
+//     .on('error', (err) => {
+//       console.log(err.message);
+//     });
+// };
+
+const basicETL = () => load.copy(pool, 'product', options.product)
+  .then(() => load.copy(pool, 'reviews', options.reviews))
+  .then(() => load.copy(pool, 'reviews_photos', options.reviews_photos))
+  .then(() => load.copy(pool, 'characteristics', options.characteristics))
+  .catch((err) => {
+    setImmediate(() => { throw err; });
+  });
+
+const updateCharETL = (fileName) => {
   extract.getInputFileStream(fileName)
     .pipe(csv.parse({ delimiter: ',', from_line: 2 }))
-    .on('data', async (row) => {
-      // query the database to add data. Returns a promise.
-      await load.insertOne(pool, row, options[fileName]);
+    .on('data', (row) => {
+      load.updateChar(pool, row, { tableName: 'hr_sdc.characteristics' });
     })
     .on('end', () => {
       console.log(`${fileName} reading finished!`);
@@ -37,6 +72,9 @@ const basicETL = (fileName) => {
     });
 };
 
-basicETL('product');
-basicETL('reviews');
-basicETL('reviews_photos');
+// basicETL('product');
+// basicETL('reviews');
+// basicETL('reviews_photos');
+// basicETL('characteristics');
+basicETL()
+  .then(() => updateCharETL('characteristic_reviews'));
--- etl_system/load.js
+++ etl_system/load.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 exports.insertOne = (pool, data, options) => {
   const valuesArr = [];
   for (let i = 1; i <= options.numOfCols; i++) {
@@ -16,4 +18,34 @@ exports.insertOne = (pool, data, options) => {
     .catch((err) => {
       setImmediate(() => { throw err; });
     });
+};
+
+exports.copy = (pool, fileName, options) => {
+  const filePath = path.join(__dirname, `examples/${fileName}.csv`);
+
+  // console.log(filePath, fileName, options);
+  const query = `
+    COPY ${options.tableName}(${options.colNames})
+    FROM '${filePath}'
+    DELIMITER ','
+    CSV HEADER;
+  `;
+
+  return pool
+    .query(query)
+    .then(() => console.log(`complete copying ${options.tableName} from ${filePath}`))
+    // .catch((err) => {
+    //   setImmediate(() => { throw err; });
+    // });
+}
+
+exports.updateChar = (pool, data, options) => {
+  const query = `UPDATE ${options.tableName}
+      SET value_total=value_total+${data[3]}, value_count=value_count+1
+      WHERE id=${data[1]}`;
+
+  return pool
+    .query(query)
+    .then(() => console.log(`${data[0]} added to ${options.tableName}`))
+    .catch((err) => { throw err; });
 };
