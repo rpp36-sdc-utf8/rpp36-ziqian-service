7730536fabe4a6c1a77d016a7a2a5ffbd1e46082
Ziqian Li <liziqian1992@gmail.com>
2022-08-15 21:23:45 -0700

    define all routes

--- primaryDB/model.js
+++ primaryDB/model.js
@@ -1,28 +1,54 @@
 const pool = require('./index');
 
-exports.fetch = (productId, options) => {
-  const {count, sort, page} = options;
-  // fetch from hr_sdc.reviews based on options
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+const fetchPhotos = (reviewId) => {
+  // fetch all photos
+};
+
+const fetchCharacteristic = (productId) => {
+  // fetch chars and construct into an obj to return
+}
+
+exports.fetchReviews = (options) => {
+  const {productId, count, sort, page} = options;
+  // fetch reviews from hr_sdc.reviews based on options
+  // get all reviews_id and look for photos for those reviews
+};
+
+exports.fetchReviewsMeta = (productId) => {
+  // query to fetch all reviews with product_id column rating, recommended
+  // query to fetch characteristics name, total, count
+  // constuct the response object
 };
 
 exports.insertOne = (productId, data) => {
+  // if data has photos
+    // reconstruct data to review data and photo data
+
+  // get characteristics
+
   // construct query string
-  const query = {
-    // text: `INSERT INTO ${tableName}(${colNames}) VALUES(${valStr})`,
-    text: `INSERT INTO ${tableName}(id,name,slogan,description,category,default_price) VALUES($1,$2,$3,$4,$5,$6)`,
+  const insertToReviews = {
+    text: `INSERT INTO hr_sdc.reviews () VALUES($1,$2,$3,$4,$5,$6)`,
     values: data,
   };
 
-  // query
   return pool
-    .query(query)
-    .then((res) => console.log(`data added to ${tableName}`))
-    .catch((err) => {
-      // console.error(err.stack);
-      setImmediate(() => { throw err; });
+    .query(insertToReviews)
+    .then((data) => {
+      // get insertId for reviews
+      // if has photos
+        // insert photos
+      // update characteristics
     });
 };
 
-exports.updateOne = (reviewId, data) => {
+exports.updateOne = (reviewId, options) => {
   // update helpfulness/report on review matching id
 };
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const Models = require('../primaryDB/index');
 
 const app = express();
 app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,27 +13,63 @@ app.get('/', (req, res) => {
 
 app.route('/reviews')
   .get((req, res) => {
-    // fetch data
-    // respond with data
+    const { product_id, page, count, sort } = req.query;
+    const options = {
+      productId: product_id,
+      page: page || 1,
+      count: count || 5,
+      sort: sort || 'relevant',
+    };
+    res.status(200).json(req.query);
+    // Models.fetchReviews(options)
+    //   .then((data) => {
+    //     res.status(200).json(data);
+    //   })
+    //   .catch((err) => res.status(500).send(err));
   })
   .post((req, res) => {
-    // insert data
-    // respond with success when insert happens
-  })
+    const body = req.body;
+    res.status(200).json(body);
 
-app.get('reviews/meta', (req, res) => {
-  // fetch reviews meta data
-  // respond with reivews meta data
+    // Models.insertOne(body)
+    //   .then(() => {
+    //     res.status(201).send('Created');
+    //   })
+    //   .catch((err) => res.status(500).send(err));
+  });
+
+app.get('/reviews/meta', (req, res) => {
+  const { product_id } = req.query;
+  res.status(200).json(req.query);
+
+  // Models.fetchReviewsMeta(product_id)
+  //   .then((data) => {
+  //     res.status(200).json(data);
+  //   })
+  //   .catch((err) => res.status(500).send(err));
 });
 
-app.put('/reviews/:review_id/helpful', (req, res) =>{
-  // update query to db
-  // send response after success update
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  const { review_id } = req.params;
+  const query = 'helpfulness';
+  res.status(200).json(req.params);
+
+  // Models.updateOne(review_id, query)
+  //   .then(() => {
+  //     res.send(201).send('Updated');
+  //   })
+  //   .catch((err) => res.status(500).send(err));
 });
 
 app.put('/reviews/:review_id/report', (req, res) => {
-  // update query to db
-  // send response after success update
+  const { review_id } = req.params;
+  const query = 'report';
+  res.status(200).json(req.params);
+  // Models.updateOne(review_id, query)
+  //   .then(() => {
+  //     res.send(201).send('Updated');
+  //   })
+  //   .catch((err) => res.status(500).send(err));
 });
 
 module.exports = app;
