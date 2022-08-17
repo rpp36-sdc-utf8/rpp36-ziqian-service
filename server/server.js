9ac2b23eb56dc67b326d35fe5321e8d7e5123e56
Ziqian Li <liziqian1992@gmail.com>
2022-08-16 22:34:56 -0700

    constructed GET /review route and query

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const Models = require('../primaryDB/index');
+const Models = require('../primaryDB/model');
 
 const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -16,60 +16,73 @@ app.route('/reviews')
     const { product_id, page, count, sort } = req.query;
     const options = {
       productId: product_id,
-      page: page || 1,
+      page: page || 0,
       count: count || 5,
-      sort: sort || 'relevant',
+      sort: sort || 'relavent',
     };
-    res.status(200).json(req.query);
-    // Models.fetchReviews(options)
-    //   .then((data) => {
-    //     res.status(200).json(data);
-    //   })
-    //   .catch((err) => res.status(500).send(err));
+
+    let data = {
+      product: product_id,
+      page: page || 0,
+      count: count || 5,
+    };
+
+    Models.fetchReviews(options)
+      .then((results) => {
+        data.results = results;
+        console.log('data', data);
+        res.status(200).json(data);
+      })
+      .catch((err) => res.status(500).json(err));
   })
   .post((req, res) => {
     const body = req.body;
-    res.status(200).json(body);
+
+    // res.status(200).json(body);
 
     // Models.insertOne(body)
     //   .then(() => {
     //     res.status(201).send('Created');
     //   })
-    //   .catch((err) => res.status(500).send(err));
+    //   .catch((err) => res.status(500).json(err));
   });
 
 app.get('/reviews/meta', (req, res) => {
   const { product_id } = req.query;
-  res.status(200).json(req.query);
+
+  // res.status(200).json(req.query);
 
   // Models.fetchReviewsMeta(product_id)
   //   .then((data) => {
   //     res.status(200).json(data);
   //   })
-  //   .catch((err) => res.status(500).send(err));
+  //   .catch((err) => res.status(500).json(err));
 });
 
 app.put('/reviews/:review_id/helpful', (req, res) => {
   const { review_id } = req.params;
   const query = 'helpfulness';
-  res.status(200).json(req.params);
+
+  // res.status(200).json(req.params);
 
   // Models.updateOne(review_id, query)
   //   .then(() => {
   //     res.send(201).send('Updated');
   //   })
-  //   .catch((err) => res.status(500).send(err));
+  //   .catch((err) => res.status(500).json(err));
 });
 
 app.put('/reviews/:review_id/report', (req, res) => {
   const { review_id } = req.params;
   const query = 'report';
-  res.status(200).json(req.params);
+
+  // res.status(200).json(req.params);
+
   // Models.updateOne(review_id, query)
   //   .then(() => {
   //     res.send(201).send('Updated');
   //   })
-  //   .catch((err) => res.status(500).send(err));
+  //   .catch((err) => res.status(500).json(err));
 });
 
 module.exports = app;
